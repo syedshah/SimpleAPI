9fe68b6bdc567ad616d07c5b458de7fc658c1aee
Syed Shah <syed.naeem@gmail.com>
2022-04-19 13:22:43 +0100

    updated YAML file

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -32,6 +32,6 @@ public class WeatherForecastController : ControllerBase
 
     [HttpGet("{id}")]
     public ActionResult<string> Get(int id){
-        return "Hello Word!";
+        return "Hello Word";
     }
 }
